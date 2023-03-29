8c6ffd8da37480b112f921318905e42956f264f3
kristel <kristel1menegue89@gmail.com>
2023-03-29 10:13:48 -0600

    se realizaron cambios

--- Laboratorio.py
+++ Laboratorio.py
@@ -1,7 +1,6 @@
 n1=int(input("Ingrese un numero:"))
 n2=int(input("Ingrese otro numero:"))
 
-
 suma= n1 + n2
 print(suma)
 
@@ -11,6 +10,5 @@ print(resta)
 multiplicacion= n1 * n2
 print(multiplicacion)
 
-division= n1/n2
+division= n1/ n2
 print(division)
-
