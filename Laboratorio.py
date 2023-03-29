9c9cb64a515f8b1d4f9b38afcd68c09dba46eb42
kristel <kristel1menegue89@gmail.com>
2023-03-29 08:45:54 -0600

    se agregraron dos lineas de ingrese un numero

---
+++ Laboratorio.py
@@ -0,0 +1,15 @@
+n1=int(input("Ingrese un numero:"))
+n2=int(input("Ingrese otro numero:"))
+
+
+suma= n1 + n2
+print(suma)
+
+resta= n1 - n2
+print(resta)
+
+multiplicacion= n1 * n2
+print(multiplicacion)
+
+division= n1/n2
+print(division)
